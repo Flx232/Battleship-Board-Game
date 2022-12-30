445622a377713fa87b38cb7294983a632644f275
Frank Xu <flx2@njit.edu>
2022-12-30 16:36:39 -0500

    revert main

--- battle_ship/__init__.py
+++ battle_ship/__init__.py
@@ -1,17 +1,5 @@
-from battle_ship.start_game import Player_Start_Game
-from battle_ship.player import Player
-from battle_ship.start_game.sim.sim_play import Bot_Player_Info
-from battle_ship.start_game.sim.sim_pre_game import Sim_Start_Game
-
-
 def main():
-    player1 = Player.create()
-    start_player = Player_Start_Game()
-    start_player.pre_game(player1)
-    player2 = Bot_Player_Info.create()
-    seed = 2
-    temp = 0
-    print('\n')
+    print("Hello World")
 
 if __name__ == "__main__":
     main()
